f7f1e751b2c331a08fd551fa76441384ad677cc8
Nejra Coric <nejracoric@Alens-MacBook-Pro.local>
2022-08-09 09:11:06 +0200

    base-init

---
+++ app/src/main/java/com/example/todoisthulapps/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.todoisthulapps
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todoisthulapps.ui.theme.TodoistHulAppsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoistHulAppsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Nejra")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TodoistHulAppsTheme {
+        Greeting("Android")
+    }
+}

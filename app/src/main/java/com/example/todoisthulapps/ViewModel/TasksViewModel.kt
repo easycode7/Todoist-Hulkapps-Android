b1c1da2abe829c58d4b0fef08a34fc615f1d8328
admirsaheta <admirxsaheta@gmail.com>
2022-08-25 10:34:39 +0200

    postmethod

--- app/src/main/java/com/example/todoisthulapps/ViewModel/TasksViewModel.kt
+++ app/src/main/java/com/example/todoisthulapps/ViewModel/TasksViewModel.kt
@@ -16,8 +16,19 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.todoisthulapps.Components.TaskCard
 import com.example.todoisthulapps.Model.Data.MockData.TaskItem
+import io.ktor.client.*
+import io.ktor.client.features.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.utils.io.core.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import org.json.JSONArray
 import java.lang.Exception
+import kotlin.io.use
 
 // ** Function to ParseJSON || Contains composable elements of TaskCards ** //
 
@@ -100,6 +111,50 @@ fun downloadArray(context: Context, taskList: MutableList<String>) {
    queue.add(request)
 }
 
+// Post Method try382
 
+object KtorClient {
+   private val json = Json {
+      encodeDefaults = true
+      ignoreUnknownKeys = true
+   }
+   val httpClient = HttpClient {
+      install(JsonFeature) {
+         serializer = KotlinxSerializer(json)
+      }
+      install(Logging) {
+         logger = object : Logger {
+            override fun log(message: String) {
+               Log.d("xapp-ktor", message)
+            }
+         }
+         level = LogLevel.ALL
+      }
+      install(HttpTimeout) {
+         socketTimeoutMillis = 15_0000
+         requestTimeoutMillis = 15_000
+         connectTimeoutMillis = 15_000
+      }
+      defaultRequest {
+         contentType(ContentType.Application.Json)
+         accept(ContentType.Application.Json)
+      }
+   }
+}
+
+suspend fun postData(): String {
+   return KtorClient.httpClient.use {
+      it.post("https://todoist-hulkapps.herokuapp.com/epics/tasks") {
+         body =  Task("Test", "Description", "High")
+      }
+   }
+}
+
+@Serializable
+data class Task(
+   val title: String,
+   val description: String,
+   val priority: String
+)
 
 
--- app/src/main/java/com/example/todoisthulapps/Views/CreateNewTaskView.kt
+++ app/src/main/java/com/example/todoisthulapps/Views/CreateNewTaskView.kt
@@ -1,39 +1,41 @@
 package com.example.todoisthulapps.Views
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.todoisthulapps.Components.ButtonCreateTask
-import com.example.todoisthulapps.Components.CustomOutlinedTextField
-import com.example.todoisthulapps.Components.TitleText
+import androidx.compose.ui.unit.sp
+import com.example.todoisthulapps.ViewModel.postData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
 
 
 @Composable
 fun CreateNewTaskView() {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .fillMaxWidth()
-        .verticalScroll(rememberScrollState())) {
-        TitleText(title = "Create New Task")
-        CustomOutlinedTextField(label = "Title", singleLineOn = true)
-        CustomOutlinedTextField(label = "Description", singleLineOn = false)
-        CustomOutlinedTextField(label = "Priority", singleLineOn = true)
-        Column(
-            modifier = Modifier
-                .padding(100.dp)
-                .fillMaxWidth()
-                .fillMaxHeight(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            ButtonCreateTask()
-        }
-
-
+    val args = produceState(initialValue = "", producer = {
+        value = postData()
+    })
+    
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
+        Text(text = args.value)
     }
 }
 
